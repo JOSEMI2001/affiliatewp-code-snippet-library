f66d73ba82e06c9207f6c42b2d0207a9567c3f4b
Andrew Munro <andrew@sumobi.com>
2017-01-31 11:36:52 +1300

    Update code

--- affiliate-area/move-login-above-register.php
+++ affiliate-area/move-login-above-register.php
@@ -1,38 +1,84 @@
 <?php
 /**
  * Plugin Name: AffiliateWP - Move Login Form Above Register Form
- * Plugin URI: http://affiliatewp.com
- * Description: Swaps the position of the login and register form on the affiliate area
+ * Plugin URI: https://affiliatewp.com
+ * Description: Swaps the position of the login and register form on the Affiliate Area
  * Author: Andrew Munro, Sumobi
  * Author URI: http://sumobi.com
- * Version: 1.0
+ * Version: 1.0.1
  */
 
-function affwp_custom_move_login_above_register() {
+/**
+ * This code snippet requires AffiliateWP v2.0+
+ */
+function affwp_custom_move_login_above_register( $atts, $content = null ) {
+
+	if ( ! function_exists( 'affiliate_wp' ) ) {
+		return;
+	}
+
+	// See https://github.com/AffiliateWP/AffiliateWP/issues/867
+	if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+		return;
+	}
+
+	affwp_enqueue_script( 'affwp-frontend', 'affiliate_area' );
+
+	/**
+	 * Filters the display of the registration form
+	 *
+	 * @since 2.0
+	 * @param bool $show Whether to show the registration form. Default true.
+	 */
+	$show_registration = apply_filters( 'affwp_affiliate_area_show_registration', true );
+
+	/**
+	 * Filters the display of the login form
+	 *
+	 * @since 2.0
+	 * @param bool $show Whether to show the login form. Default true.
+	 */
+	$show_login = apply_filters( 'affwp_affiliate_area_show_login', true );
+
 	ob_start();
 
 	if ( is_user_logged_in() && affwp_is_affiliate() ) {
 		affiliate_wp()->templates->get_template_part( 'dashboard' );
-	} elseif( is_user_logged_in() && affiliate_wp()->settings->get( 'allow_affiliate_registration' ) ) {
-		affiliate_wp()->templates->get_template_part( 'register' );
+	} elseif ( is_user_logged_in() && affiliate_wp()->settings->get( 'allow_affiliate_registration' ) ) {
+
+		if ( true === $show_registration ) {
+			affiliate_wp()->templates->get_template_part( 'register' );
+		}
+
 	} else {
 
 		if ( ! is_user_logged_in() ) {
-			affiliate_wp()->templates->get_template_part( 'login' );
+
+			if ( true === $show_login ) {
+				affiliate_wp()->templates->get_template_part( 'login' );
+			}
+
 		}
 
 		if ( affiliate_wp()->settings->get( 'allow_affiliate_registration' ) ) {
-			affiliate_wp()->templates->get_template_part( 'register' );
+
+			if ( true === $show_registration ) {
+				affiliate_wp()->templates->get_template_part( 'register' );
+			}
+
 		} else {
 			affiliate_wp()->templates->get_template_part( 'no', 'access' );
 		}
 
+
+
 	}
 
 	return ob_get_clean();
+
 }
 
 $affiliate_wp = function_exists( 'affiliate_wp' ) ? affiliate_wp() : '';
 
 remove_shortcode( 'affiliate_area', array( $affiliate_wp, 'affiliate_area' ) );
-add_shortcode( 'affiliate_area', 'affwp_custom_move_login_above_register' );
+add_shortcode( 'affiliate_area', 'affwp_custom_move_login_above_register', 10, 2 );
